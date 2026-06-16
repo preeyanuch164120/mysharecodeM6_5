e41efb22868ff0843db8eccce59aa672587d804e
preeyanuch164120 <164120@wsk.ac.th>
2026-06-16 04:55:30 +0000

    first01.py

---
+++ first01.py
@@ -0,0 +1,6 @@
+print("โปรแกรมคำนวนพื้นที่ของสามเหลี่ยม")
+h=float(input("enter hight:"))
+b=float(input("enter base:"))
+area=0.5*h*b
+print("พื้นที่ของสามเหลี่ยม",area)
+print("จัดทำโดย นางสาวปรีญานุช ดอกพวง เลขที่18")
